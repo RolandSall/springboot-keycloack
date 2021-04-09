a7ff7cbd9442885b5b1950d0dea48dfeab16fc34
Roland Salloum <roland.salloum00@outlook.com>
2021-04-09 15:04:11 +0200

    chore: initial project setup

---
+++ src/test/java/com/rolandsall/springbootkeycloak/SpringbootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
